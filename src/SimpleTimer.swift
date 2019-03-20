80ae4f2ed680717431ac2b9811c7c596688ef471
Eonist <30n1st@gmail.com>
2019-03-20 15:54:25 +0100

    Improve core functionality

--- src/SimpleTimer.swift
+++ src/SimpleTimer.swift
@@ -1,25 +1,40 @@
 import Foundation
 /**
- * EXAMPLE: SimpleTimer(interval: 3,repeats: true){print("tick")}.start()//ticks every 3 seconds
- * IMPORTANT: ⚠️️ @objc func update() {Swift.print("tick")}//method must be in the public or scope
- * IMPORTANT: ⚠️️ because your object has a property to store the timer, and the timer calls a method on the object, you have a strong reference cycle that means neither object can be freed. To fix this, make sure you invalidate the timer when you're done with it, such as when your view is about to disappear: timer.invalidate()
+ * - Important: ⚠️️ @objc func update() {Swift.print("tick")}//method must be in the public or scope
+ * - Important: ⚠️️ because your object has a property to store the timer, and the timer calls a method on the object, you have a strong reference cycle that means neither object can be freed. To fix this, make sure you invalidate the timer when you're done with it, such as when your view is about to disappear: timer.invalidate()
+ * ## Examples:
+ * SimpleTimer(interval:3, repeats:true){print("tick")}.start()//ticks every 3 seconds
  */
 public class SimpleTimer {
    private var timer:Timer?
-   var interval:TimeInterval /*in seconds*/
-   private var repeats:Bool
+   public var interval:TimeInterval /*in seconds*/
+   public var repeats:Bool
    public var tick:Tick
-   public init( interval:TimeInterval, repeats:Bool = false, onTick:@escaping Tick){
+   public init(interval:TimeInterval, repeats:Bool = false, onTick:@escaping Tick){
       self.interval = interval
       self.repeats = repeats
       self.tick = onTick
    }
+}
+/**
+ * Core
+ */
+extension SimpleTimer{
+   /**
+    * Starts the timer
+    */
    public func start(){
-      timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(update), userInfo: nil, repeats: true) 
+      timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(update), userInfo: nil, repeats: true)
    }
+   /**
+    * Stops the timer
+    */
    public func stop(){
-      if let timer = timer {timer.invalidate()}
+      timer?.invalidate()
    }
+   /**
+    * Resets the timer
+    */
    public func reset(){
       stop()
       start()
@@ -27,13 +42,16 @@ public class SimpleTimer {
    /**
     * This method must be in the public or scope
     */
-   @objc func update() {
+   @objc internal func update() {
       tick()
    }
 }
 /**
- * Signature
+ * Type
  */
 extension SimpleTimer {
+   /**
+    * Callback type (Equivilent to onChange)
+    */
    public typealias Tick = () -> Void
 }
